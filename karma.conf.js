2b9a940fffce50eaed294f9dd4933851dce7adf4
FXavier <francisco.jaramillo01@epn.edu.ec>
2021-10-02 14:04:22 -0500

    Despliegue

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/RentAppWeb'),
+      dir: require('path').join(__dirname, './coverage/AppetWeb'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
